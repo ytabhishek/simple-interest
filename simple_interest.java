1824a97113d566640790f7146c472558093f6e17
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:32:28 +0530

    Add files via upload

---
+++ simple_interest.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class simple_interest {
+    public static void main(String[] args) {
+        double p = 100;
+        double r = 20;
+        double t = 3;
+        double si = (p*r*t)/100;
+        System.out.println("The simple interesrt is : "+si);
+    }
+}
